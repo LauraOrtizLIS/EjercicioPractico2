ea8048da62ed1247bb2dea1e43331412b3bc9d00
Laura Ortiz Lavín <laura.ortiz@lisdatasolutions.com>
2025-12-01 18:23:53 +0100

    Añadir implemetación 1 a C

--- main.py
+++ main.py
@@ -5,12 +5,16 @@ from astronomia.planetas import planetas
 from astronomia.galaxias import galaxias
 
 
+def explora(a):
+    sitios_a_explorar = ["rio", "lago", "campo"]
+    print(f"el alien {a} explora {random.choice(sitios_a_explorar)}")
+
 def cosmos(a, p, g):
     for alien in a:
         print(
-            f"El {alien} del planeta {random.choce(p)} de la galaxia {random.choice(g)}"
+            f"El {alien} del planeta {random.choice(p)} de la galaxia {random.choice(g)}"
         )
-
+    explora(a)
 
 """
 Rutina/Funcion principal
